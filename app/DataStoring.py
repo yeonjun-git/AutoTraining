6c01a1c1543b1b5dbfa2d445ce138b2730635524
yeonjun-git <lyj09029@gmail.com>
2023-04-21 16:31:42 +0900

    progressing

---
+++ app/DataStoring.py
@@ -0,0 +1,35 @@
+
+# 1. Crawling Data
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from Module.Crawling import Dataset
+NewsData = Dataset()
+page = 10
+per_page = 10
+title, link, desc, pubdate = NewsData.DataList("금리", page, per_page)
+
+
+
+# 2. Connect Postgres
+from Module.config import get_secret
+password = get_secret("postgresDB_password")
+
+import  psycopg2
+postgresDB = psycopg2.connect(
+    host = 'localhost', dbname = 'postgres',
+    user = 'postgres', password = password, port = 5432
+    )
+
+cursor = postgresDB.cursor()
+
+
+# 3. Storing data to DB
+for idx in range(page*per_page):
+    cursor.execute(
+        "insert into airflow_pjt.news_data values (%s, %s, %s, %s, %s);",
+        (idx, title[idx], link[idx], desc[idx], pubdate[idx])
+    )
+    
+postgresDB.commit()
+
